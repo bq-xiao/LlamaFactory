143df3c0f937acd848964d2de3758924b0f3c01c
肖保强 <xiaobq@shuhaisc.com>
2026-05-07 10:00:14 +0800

    本机训练微调

--- data2message.py
+++ data2message.py
@@ -2,18 +2,26 @@ import json
 
 def data2message(in_file, out_file):
     messages = []
+    messages.append({
+        "role": "system",
+        "content": "我是你的问答助手"
+    })
     with open(in_file, 'r', encoding='utf-8') as file:
         for doc in file:
             if "NaN" in doc:
                 doc = doc.replace("NaN", "\"？\"")
             conversations = json.loads(doc)
-            messages.extend(conversations['conversations'])
+            for conv in conversations['conversations']:
+                messages.append({
+                    "role":conv['from'],
+                    "content": conv['value']
+                })
     data = {
-        "conversations": messages
+        "messages": messages
     }
-    msg = json.dumps(data)
-    print(msg)
+    #msg = json.dumps(data)
+    #print(msg)
     with open(out_file, 'w', encoding='utf-8') as f:
         f.write(json.dumps(data, ensure_ascii=False))
 
-data2message('multi_test.jsonl', 'test.json')
+data2message('multi_test.jsonl', 'qwen_train_test.json')
